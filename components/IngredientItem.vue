c8145bc36ae670c532c3dc0e295fd458fb869c6d
laszlbe <bence.laszlo@dachs.ch>
2019-09-28 12:11:46 +0200

    (components/IngredientItem.vue): Implement selection functionality.

--- components/IngredientItem.vue
+++ components/IngredientItem.vue
@@ -3,8 +3,9 @@
     <span>id: {{ id }}</span>
     <img :src="pic" alt="ingredient" />
     <span>{{ name }}</span>
-    <span v-if="isSelected">selected</span>
+    <span v-if="isSelected">✅</span>
     <span v-else></span>
+    <button @click="selectItem" class="select-btn">Select</button>
   </div>
 </template>
 
@@ -36,8 +37,22 @@ export default {
       pic: this.initialPic,
       isSelected: this.initialIsSelected
     }
+  },
+
+  methods: {
+    selectItem() {
+      if (this.isSelected) {
+        this.isSelected = false
+      } else {
+        this.isSelected = true
+      }
+    }
   }
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+.select-btn {
+  margin: 1px red solid;
+}
+</style>
