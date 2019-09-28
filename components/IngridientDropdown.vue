e318eaaddb489628762761753c90d5562f1b37c7
Gabor Javora <gabor.javora@dachs.ch>
2019-09-28 12:25:48 +0200

    Implemented IngridientDropdown

---
+++ components/IngridientDropdown.vue
@@ -0,0 +1,26 @@
+<template>
+  <select>
+    <option
+      v-for="ingredient in ingredients"
+      :key="ingredient.id"
+      :value="ingredient.name"
+    >
+      {{ ingredient.name }}
+    </option>
+  </select>
+</template>
+
+<script>
+import { ingredientData } from '@/api/data'
+
+export default {
+  data() {
+    return {
+      ingredients: []
+    }
+  },
+  async mounted() {
+    this.ingredients.push(...(await ingredientData()))
+  }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,17 @@
 <template>
   <div class="container">
+    <IngridientDropdown />
     <IngredientList />
   </div>
 </template>
 
 <script>
+import IngridientDropdown from '@/components/IngridientDropdown'
 import IngredientList from '@/components/IngredientList'
 
 export default {
   components: {
+    IngridientDropdown,
     IngredientList
   }
 }
